ae40c97785d0be8d74c1cd3d3bb939a28bf75bba
Gabriel Fontes <eu@misterio.me>
2021-12-17 11:39:02 -0300

    remove leftover ssh pub key

--- configuration.nix
+++ configuration.nix
@@ -24,11 +24,11 @@
 
   # TODO: Configure your users as usual.
   users.users = {
+    # TODO: Replace with your username
     you = {
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
-        # Add your SSH public key(s) here.
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDci4wJghnRRSqQuX1z2xeaUR+p/muKzac0jw0mgpXE2T/3iVlMJJ3UXJ+tIbySP6ezt0GVmzejNOvUarPAm0tOcW6W0Ejys2Tj+HBRU19rcnUtf4vsKk8r5PW5MnwS8DqZonP5eEbhW2OrX5ZsVyDT+Bqrf39p3kOyWYLXT2wA7y928g8FcXOZjwjTaWGWtA+BxAvbJgXhU9cl/y45kF69rfmc3uOQmeXpKNyOlTk6ipSrOfJkcHgNFFeLnxhJ7rYxpoXnxbObGhaNqn7gc5mt+ek+fwFzZ8j6QSKFsPr0NzwTFG80IbyiyrnC/MeRNh7SQFPAESIEP8LK3PoNx2l1M+MjCQXsb4oIG2oYYMRa2yx8qZ3npUOzMYOkJFY1uI/UEE/j/PlQSzMHfpmWus4o2sijfr8OmVPGeoU/UnVPyINqHhyAd1d3Iji3y3LMVemHtp5wVcuswABC7IRVVKZYrMCXMiycY5n00ch6XTaXBwCY00y8B3Mzkd7Ofq98YHc= (none)"
+        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
     };
   };
