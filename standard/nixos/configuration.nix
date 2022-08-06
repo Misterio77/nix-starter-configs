5981a6010b1f5f99cb0592980d975d9b6ec837b9
Gabriel Fontes <eu@misterio.me>
2022-08-05 22:55:29 -0300

    remove profile activation, add channel sync to default nixos config

--- standard/nixos/configuration.nix
+++ standard/nixos/configuration.nix
@@ -18,15 +18,15 @@
     # You can also split up your configuration and import pieces of it here.
   ];
 
-  # This will add your inputs as registries, making operations with them (such
-  # as nix shell nixpkgs#name) consistent with your flake inputs.
-  nix.registry = lib.mapAttrs' (n: v: lib.nameValuePair n { flake = v; }) inputs;
+  nix = {
+    # This will add each flake input as a registry
+    # To make nix3 commands consistent with your flake
+    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
 
-  # Will activate home-manager profiles for each user upon login
-  # This is useful when using ephemeral installations
-  environment.loginShellInit = ''
-    [ -d "$HOME/.nix-profile" ] || /nix/var/nix/profiles/per-user/$USER/home-manager/activate &> /dev/null
-  '';
+    # This will additionally add your inputs to the system's legacy channels
+    # Making legacy nix commands consistent as well, awesome!
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+  };
 
   nix = {
     settings = {
