5b996debfa319ccf7ed10ddfa361cc10e80af324
Gabriel Fontes <eu@misterio.me>
2022-08-05 22:52:49 -0300

    add shameless plug about nix-colors

--- standard/flake.nix
+++ standard/flake.nix
@@ -11,6 +11,10 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # TODO: Add any other flake you might need
+
+    # Shameless plug: looking for a way to nixify your themes and make
+    # everything match nicely? Try nix-colors!
+    # nix-colors.url = "github:misterio77/nix-colors";
   };
 
   outputs = { nixpkgs, home-manager, ... }@inputs:
