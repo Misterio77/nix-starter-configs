f0e313c88666315485e7d39d821147b1745e5386
Gabriel Fontes <hi@m7.rs>
2023-02-20 17:08:47 -0300

    add nixpkgs-unstable to standard template
    I decided it is a common enough usecase, so it makes sense to include it
    
    Co-authored-by: Will Forman <wf8581@gmail.com>

--- standard/flake.nix
+++ standard/flake.nix
@@ -4,6 +4,10 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    # You can access packages and modules from different nixpkgs revs
+    # at the same time. Here's an working example:
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
     # Home manager
     home-manager.url = "github:nix-community/home-manager";
@@ -43,7 +47,7 @@
       );
 
       # Your custom packages and modifications, exported as overlays
-      overlays = import ./overlays;
+      overlays = import ./overlays { inherit inputs; };
       # Reusable nixos modules you might want to export
       # These are usually stuff you would upstream into nixpkgs
       nixosModules = import ./modules/nixos;
--- standard/overlays/default.nix
+++ standard/overlays/default.nix
@@ -1,4 +1,5 @@
 # This file defines overlays
+{ inputs, ... }:
 {
   # This one brings our custom packages from the 'pkgs' directory
   additions = final: _prev: import ../pkgs { pkgs = final; };
@@ -10,5 +11,14 @@
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
+  };
+
+  # When applied, the unstable nixpkgs set (declared in the flake inputs) will
+  # be accessible through 'pkgs.unstable'
+  unstable-packages = final: _prev: {
+    unstable = import inputs.nixpkgs-unstable {
+      system = final.system;
+      config.allowUnfree = true;
+    };
   };
 }
