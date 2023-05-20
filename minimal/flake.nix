54526bbdf42d9b4fc2097cb431e92f2e6d509127
Dmytro Kyrychuk <dmytro@kyrych.uk>
2023-05-20 12:57:52 +0000

    Sync home-manager and nixpkgs branches

--- minimal/flake.nix
+++ minimal/flake.nix
@@ -6,7 +6,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-22.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # TODO: Add any other flake you might need
