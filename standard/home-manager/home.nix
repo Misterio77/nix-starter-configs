8b0809c52b2e88a122f23bd4cc6ecd39196a0ced
Gabriel Nützi <gnuetzi@gmail.com>
2023-11-11 18:35:02 +0100

    fix: Workaround for  `allowUnfreePredicate` is solved
    I think this workaround can be removed. I am however not sure
    in which version of home-manager that came in.
    
    Probably we cannot yet merge this since its not in `release-23.05` branch as this templates refer to.
    
    https://github.com/nix-community/home-manager/pull/4304

--- standard/home-manager/home.nix
+++ standard/home-manager/home.nix
@@ -42,8 +42,6 @@
     config = {
       # Disable if you don't want unfree packages
       allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = _: true;
     };
   };
 
