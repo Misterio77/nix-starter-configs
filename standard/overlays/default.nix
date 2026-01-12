e633d8d31ef214be4a52d0e775e1c157315f56fb
Linus Emmerich <96724651+linusemr618@users.noreply.github.com>
2026-01-12 10:20:52 +0100

    Fix deprecated system attribute to avoid evaluation warnings on 25.11

--- standard/overlays/default.nix
+++ standard/overlays/default.nix
@@ -16,7 +16,7 @@
   # be accessible through 'pkgs.unstable'
   unstable-packages = final: _prev: {
     unstable = import inputs.nixpkgs-unstable {
-      system = final.system;
+      system = final.stdenv.hostPlatform.system;
       config.allowUnfree = true;
     };
   };
